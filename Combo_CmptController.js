fb651ea197f3da15b798c2c9c77ec15ccc885376
deepakinst22 <deepak.inst22@gmail.com>
2018-12-29 17:09:37 +0530

    Create Combo_CmptController.js

---
+++ Combo_CmptController.js
@@ -0,0 +1,10 @@
+({
+     init: function (cmp, event, helper) {         
+        helper.getContactList(cmp);        
+    },
+    handleChange: function (cmp, event) {
+        // This will contain the string of the "value" attribute of the selected option
+        var selectedOptionValue = event.getParam("value");
+        alert("Option selected with value: '" + selectedOptionValue + "'");
+    }
+})
